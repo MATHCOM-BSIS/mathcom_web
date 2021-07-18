76a9479faf5a99856695321d76c96ba21c6cce08
TH_PHEC <59645009+gimon0330@users.noreply.github.com>
2021-07-19 00:43:46 +0900

    First Upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+      <div className = "black-nav">
+        <div>부산일과학고등학교 인공지능 동아리 - MATHCOM</div>
+      </div>
+      <header className="App-header">
+        <h1>
+          Hello
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
